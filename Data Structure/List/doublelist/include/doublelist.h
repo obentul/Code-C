7574d1e1d2e96297acd0a41258d6cbcaedf9d19e
yk <yk@localhost.localdomain>
2018-06-24 23:34:47 +0800

    xx

--- Data Structure/List/doublelist/include/doublelist.h
+++ Data Structure/List/doublelist/include/doublelist.h
@@ -1,3 +1,6 @@
+#ifndef _DOUBLELIST_H_
+#define _DOUBLELIST_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -21,3 +24,5 @@ int reverselink(struct double_link *tmp,struct double_link **new);
 //todo
 int deletenode_by_value(struct double_link *tmp,int value);
 int sortlink(struct double_link *tmp,struct double_link *new,int orient);
+
+#endif
--- Data Structure/List/doublelist/src/doublelist.c
+++ Data Structure/List/doublelist/src/doublelist.c
@@ -1,32 +1,5 @@
 #include "doublelist.h"
 
-int main(int argc,char ** argv)
-{
-	struct double_link *tmp;
-	initlink(&tmp,5,8);
-	printflink(tmp);
-	printf("%d\n",lenlink(tmp));
-	printf("\n");
-	
-	printf("insert a node to the 3rd node's left value=9:\n");
-	insertlink(tmp,3,-1,9);
-	printflink(tmp);
-	//printf("LEN=%d\n",lenlink(tmp));
-	printf("\n");
-	
-	printf("insert a node to the 1st node's left value=9:\n");
-	insertlink(tmp,1,-1,9);
-	printflink(tmp);
-	//printf("LEN=%d\n",lenlink(tmp));
-	printf("\n");
-	
-	printf("delete the first node from link list:\n");
-	deletenode_by_seq(tmp,1);
-	printflink(tmp);
-	printf("\n");
-	
-	return 0;
-}
 
 int initlink(struct double_link **out,int total,int value)
 {
