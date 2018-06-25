80eb1d9e65a05ae8e75c48b173fb2ba27fe6898d
Obt-Young <ykun089@163.com>
2018-06-25 14:10:18 +0800

    Create mythread.h

---
+++ Multi Thread/include/mythread.h
@@ -0,0 +1,31 @@
+#ifndef _MYTHREAD_H_
+#define _MYTHREAD_H_
+
+
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define CLEANUP_MAX 3
+
+struct thread_stu;
+
+typedef void vvx_func(void *args);   //入参void，返回void *
+typedef void* vxvx_func(void *args);   //入参void*，返回void *
+typedef void vv_func(void args);   //入参void，返回void
+
+struct thread_stu{
+        pthread_t thread_tid;           //线程id
+        void *thread_ret;               //返回值
+        char *thread_desc;              //线程描述
+        struct thread_stu *args;                        //创建线程入参
+        void *cleanup_args[CLEANUP_MAX];                //清理函数入参
+
+
+        vxvx_func *thread_func; //线程执行函数
+        vvx_func *thread_cleanup[CLEANUP_MAX];     //线程退出清理函数,指针数组
+};
+
+
+#endif
