fc8db006509ae704ec7acd7821c8ed74972c0ed8
root <root@localhost.localdomain>
2018-11-02 08:57:00 +0800

    增加滚屏 MISC/ScreenRolling.c

---
+++ MISC/ScreenRolling.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc,char ** argv)
+{
+    while(1){
+        int i=0;
+        for(i=0;i<10;i++){
+            printf("%d\n",i);
+            system("clear");
+        }
+    }
+}
